84fe36a3e906ff6abe95cd0d39588b9377ebe2d2
Rad Cirskis <nad2000@gmail.com>
2018-01-08 02:09:27 +1300

    ...

--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,3 +1,27 @@
 fn main() {
     println!("Hello, world!");
+
+    // Variables...
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    // shadwowing:
+    let x = x + 42;
+    println!("The value of x is: {}", x);
+
+
+    println!("Difference: {}", 56.7 / 32.2);
+
+
+    let condition = true;
+    // using 'if' as an expression:
+    let number = if condition {
+        5
+    } else {
+        6
+    };
+
+    println!("The value of number is: {}", number);
 }
